655dfa2aabdb7a8abe49996a9e83a8565e1227d6
emreerkmen <emre.erkmen@hotmail.com>
2021-01-30 00:27:58 +0300

    Service layer is added for Student.

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -11,14 +11,16 @@ import java.util.List;
 @RestController
 @RequestMapping(path = "api/v1/student")
 public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+
     @GetMapping
     public List<Student> getStudents(){
-        return List.of(new Student(
-                1L,
-                "Emre",
-                "emre.erkmen@hotmail.com",
-                LocalDate.of(2000, Month.JANUARY,5),
-                21
-        ));
+        return  studentService.getStudents();
     }
 }
